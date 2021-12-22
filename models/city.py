7408685148bc0e09bea033421df1875f10177e55
B1ueberry121 <christianlaboy839@gmail.com>
2021-12-22 17:56:41 +0000

    going back a commit

--- models/city.py
+++ models/city.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """ This class if for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    # initialize class for file/db storage type
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place'), cascade='all, delete', backref='cities')
+class City(BaseModel):
+    """ The city class, contains state ID and name """
+    state_id = ""
+    name = ""
--- models/state.py
+++ models/state.py
@@ -1,37 +1,8 @@
 #!/usr/bin/python3
-""" This is the State class """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import environ
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """ State class 
-    Attributes:
-        __tablename__: name of MySQL table
-        name: input name
-    """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    if environ['HBNB_TYPE_STORAGE'] == 'db':
-        cities = relationship('City', cascade='all, delete', backref='state')
-    else:
-        @property
-        def cities(self):
-            """ Getter method for cities
-            Return: list of cities with state_id equal to self.id
-            """
-            from models import storage
-            from models.city import City
-            #return list of City objs in __objects
-            cities_dict = storage.all(City)
-            cities_list = []
-
-            #copy values from dict to list
-            for city in cities_dict.values():
-                if city.state_id == self.id:
-                    cities_list append(city)
-
-            return cities_list
+class State(BaseModel):
+    """ State class """
+    name = ""
