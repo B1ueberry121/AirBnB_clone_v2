0ab4d8399c8a96c007159c1a34747a5f361b7993
B1ueberry121 <christianlaboy839@gmail.com>
2022-01-13 18:55:31 +0000

    adding do_pack

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,8 +5,22 @@ using the function do_deploy
 '''
 
 from fabric.api import put, run, env
-from os.path import exists
+from datetime import datetime
+from os.path import exists, isdir
 env.hosts = ['34.138.133.115', '52.90.2.123']
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
 
 
 def do_deploy(archive_path):
