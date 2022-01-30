d38de6c687ad9a20ee32c3a0399a7961c532eb19
B1ueberry121 <christianlaboy839@gmail.com>
2022-01-30 23:38:52 +0000

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' Module that start's a Flask dev server '''
+from flask import Flask, render_template, g
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
